a6294d11c45051f746fd8993b0be250c8186804d
steam0r <stephan@undev.de>
2026-08-13 17:44:01 +0200

    wip, local patchbuild

--- gulpfile.js
+++ gulpfile.js
@@ -10,13 +10,12 @@ import {optJson} from "./optjson.js";
 function _export_patch(done) {
   const cables=new Cables()
     cables.export({
-
-    "url":"https://local.cables.local",
-        "patch": "hgLqCr",
-        "destination": "patch",
-        "m": true,
-        "combinejs": false,
-        "apikey":"7bf3b8d08127602c0ac0230bf30fdc06325134f4cccf975a6a68a4ac02805da1eda4e9bc69353e368162dedb077541e2"
+        "url":"https://dev.cables.gl",
+        "patch": "uyywZr",
+        "type": "patch",
+        "destination": "export",
+        "allops": true,
+        "apikey":"95059a7d927ac0ca89c440fc68574268076c37cfffcd257afa0b08b378c68ff58bcea44ea5f169e24c77eb8e115cc5a3"
         // "dev": true
     }
       ).then((r) => {
@@ -37,7 +36,7 @@ function _combine_js() {
 }
 
 function _run_webpack(done) {
-    webpack(webpackConfig(), (err, stats) => {
+  webpack(webpackConfig(), (err, stats) => {
         if (err) throw err;
         if (stats.hasErrors()) {
             done(stats);
@@ -62,10 +61,12 @@ function _run_websqz(done) {
 
 const fetch = gulp.series(_export_patch)
 // const crunch = gulp.series(_combine_js, _run_webpack, _run_optJson,_run_websqz);
-const crunch = gulp.series(_combine_js, _run_webpack, _run_websqz);
+const crunch = gulp.series(_run_webpack, _combine_js, _run_websqz);
 const build = gulp.series(fetch, crunch);
+const squeeze = gulp.series(_run_websqz);
 export {
     build,
     fetch,
-    crunch
+    crunch,
+    squeeze
 }
--- webpack.config.js
+++ webpack.config.js
@@ -2,49 +2,65 @@ import TerserPlugin from "terser-webpack-plugin";
 import path, { dirname } from "path";
 import { BundleAnalyzerPlugin } from "webpack-bundle-analyzer";
 import { fileURLToPath } from "url";
+import cablesWebpackConfig from "@cables/cables/src/webpack/webpack.config.js";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 const plugins=[];
 plugins.push(new BundleAnalyzerPlugin({ "analyzerMode": "static", "openAnalyzer": false, "reportTitle": "cables core", "reportFilename": path.join(__dirname, "dist", "report_core.html") }));
 
 export default () => {
-    return {
-        "plugins": plugins,
-        "mode": "production",
-        "entry": "./dist/patch.js",
-        "output": {
-            "filename": "patch.js",
-        },
-        "optimization": {
-            "concatenateModules": true,
-            "minimizer": [new TerserPlugin({
-                "extractComments": false,
-                "terserOptions": {
-                    "output": {"comments": false},
-                    "toplevel": true
+  const cablesConfigs = cablesWebpackConfig({
+    "mode": "production",
+    "entry": "./export/quiet_board.cables",
+    "output": {
+      "path": path.resolve("./patch/")
+    },
+    "options": {
+      "combinejs": true,
+      "minify": true
+    },
+    "plugins": plugins
+  });
+  return [
+    ...cablesConfigs,
+    {
+      "plugins": plugins,
+      "dependencies": cablesConfigs.map((c) => { return c.name; }),
+      "mode": "production",
+      "entry": "./patch/js/patch.js",
+      "output": {
+        "filename": "patch.js",
+      },
+      "optimization": {
+        "concatenateModules": true,
+        "minimizer": [new TerserPlugin({
+          "extractComments": false,
+          "terserOptions": {
+            "output": { "comments": false },
+            "toplevel": true
+          }
+        })],
+        "minimize": true,
+        "usedExports": true
+      },
+      "module": {
+        "rules": [
+          { "sideEffects": false },
+          {
+            "test": /\.js$/,
+            "enforce": 'pre',
+            "use": [
+              {
+                "loader": "webpack-strip-blocks",
+                "options": {
+                  "blocks": ["minimalcore"],
+                  "start": '/*',
+                  "end": '*/'
                 }
-            })],
-            "minimize": true,
-            "usedExports": true
-        },
-        "module": {
-            "rules": [
-                {"sideEffects": false},
-                {
-                    "test": /\.js$/,
-                    "enforce": 'pre',
-                    "use": [
-                        {
-                            "loader": "webpack-strip-blocks",
-                            "options": {
-                                "blocks": ["minimalcore"],
-                                "start": '/*',
-                                "end": '*/'
-                            }
-                        }
-                    ]
-                }
+              }
             ]
-        },
-    }
+          }
+        ]
+      },
+    }];
 }
