66f376610be4aca482702bcc38336398c3b11da0
steam0r <stephan@undev.de>
2026-08-18 13:45:42 +0200

    export, combine locally, minify, remove minimalcore, websqz

--- buildfrommult.js
+++ buildfrommult.js
@@ -1,5 +1,5 @@
-import fs from "fs"    
-import path from "path"    
+import fs from "fs"
+import path from "path"
 import {optJson} from "./optjson.js"
 
 
@@ -15,7 +15,7 @@ export function mergeall()
     const jsonFile = path.resolve(path.join(targetDir, jsonFileName));
     let proJson = fs.readFileSync(jsonFile);
 
-    proJson=optJson(jsonFile);
+    // proJson=optJson(jsonFile);
     // proJson=JSON.stringify(optJson(JSON.parse(proJson)));
 
     const opsCode = fs.readFileSync(path.join(targetDir, "ops.js"));
@@ -23,52 +23,42 @@ export function mergeall()
 
     let jsCode = "";
 
-
     jsCode += fs.readFileSync(path.join(targetDir, "cables.js"));
+    jsCode += "\n";
+    jsCode += "CABLES.exportedPatch="+proJson+";";
     jsCode += "\n";
     jsCode += opsCode;
     jsCode += "\n";
-    // jsCode += "if(!CABLES.exportedPatches) CABLES.exportedPatches={};";
-    // jsCode += "CABLES.exportedPatches['" + shortId + "']=" + proJson + ";";
-jsCode+="\nconsole.log(1234);"
-    jsCode += "CABLES.exportedPatch="+proJson+";";
-    // jsCode += "\n";
-    // jsCode += "if(!CABLES.exportedPatch){CABLES.exportedPatch=CABLES.exportedPatches['" + shortId + "']}";
-
-jsCode+="console.log(CABLES.OPS);\n"
-
-// jsCode+="window.CABLES.OPS=Ops;\n"
-    jsCode += "window.addEventListener('load', function(event) {\n";
-    jsCode += "CABLES.jsLoaded=new Event('CABLES.jsLoaded');\n";
-    jsCode += "document.dispatchEvent(CABLES.jsLoaded);\n";
-    jsCode += "});\n";
-
-
     jsCode += fs.readFileSync(path.join(targetDir, "minigpu.js"));
     jsCode += fs.readFileSync(path.join(targetDir, "shadergraph.js"));
 
-jsCode+="function showError(initiator,...args)"
-jsCode+="{"
-jsCode+="CABLES.logErrorConsole( initiator , ...args);"
-jsCode+="}\n"
-
-jsCode+="console.log(CABLES.OPS);\n"
-jsCode+="CABLES.patch = new CABLES.Patch({ "
-jsCode+="    patch: CABLES.exportedPatch,"
-jsCode+="    'prefixAssetPath': '',"
-jsCode+="    'glCanvasResizeToWindow': true,"
-jsCode+="    'onError': showError"
-// jsCode+="    'onPatchLoaded': patchInitialized,"
-// jsCode+="    'onFinishedLoading': patchFinishedLoading"
-jsCode+="});"
     jsCode = jsCode.replaceAll(/[\u2028]/g, " ");
     jsCode = jsCode.replaceAll(/[\u2029]/g, " ");
     jsCode = jsCode.replaceAll(/[\u00A0]/g, " ");
 
+    jsCode += 'function showError(initiator,...args)\n';
+    jsCode += '{\n';
+    jsCode += '    CABLES.logErrorConsole("[" + initiator + "]", ...args);\n';
+    jsCode += '}\n';
+    jsCode += '\n';
+    jsCode += 'window.CABLES=CABLES;\n';
+    jsCode += 'window.Ops =Ops;\n';
+    jsCode += '\n';
+    jsCode += 'CABLES.patch = new CABLES.Patch({\n';
+    jsCode += '    patch: CABLES.exportedPatch,\n';
+    jsCode += '    "prefixAssetPath": "",\n';
+    jsCode += '    "assetPath": "assets/",\n';
+    jsCode += '    "jsPath": "js/",\n';
+    jsCode += '    "glCanvasResizeToWindow": true,\n';
+    jsCode += '    "onError": showError,\n';
+    jsCode += '});\n';
 
+    if (!fs.existsSync("dist/")){
+        fs.mkdirSync("dist/");
+    }
     fs.writeFileSync(path.join(targetDir, "complete.js"), jsCode);
     fs.writeFileSync(path.join("dist/", "patch.js"), jsCode);
 
-console.log("finished",jsCode.length)
+    console.log("finished", jsCode.length);
 
 }
